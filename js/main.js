4c025d778e20fbbbb0569cf95001730647a61d53
SofiaGenchi <sofigen16@gmail.com>
2025-05-16 11:07:14 -0300

    reutilizando codigo desde js para nav y footer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,49 @@
+// hacer nav X
+let headerHTML = document.querySelector('header');
+let navHTML = `
+    <div class="header-content">
+            <h1>CotiDolar</h1>
+            <nav>
+                <a href="index.html" class="active">Inicio</a>
+                <a href="noticias.html">Noticias</a>
+            </nav>
+    </div>
+`
+headerHTML.innerHTML = navHTML;
+
+// hacer footer X
+let footerHTML = document.querySelector('footer');
+let divFooter = `
+     <div class="footer-content">
+            <div class="footer-section">
+                <h3>CotiDolar AR</h3>
+                <p>Seguimiento actualizado del dolar en Argentina</p>
+            </div>
+
+            <div class="footer-section">
+                <h4>Enlaces</h4>
+                <ul>
+                    <li><a href="index.html">Inicio</a></li>
+                    <li><a href="noticias.html">Noticias</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-section">
+                <h4>Contacto</h4>
+                <p>Sofia Ailen Genchi</p>
+                <div class="social-links">
+                    <a href="https://github.com/SofiaGenchi" target="_blank"><i class="fab fa-github"></i></a>
+                    <a href="https://www.linkedin.com/in/sofiagenchi/" target="_blank"><i class="fab fa-linkedin"></i></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer-bottom">
+            <p>&copy; 2025 CotiDolar AR - Todos los derechos reservados</p>
+        </div>
+`
+footerHTML.innerHTML = divFooter
+
 fetch('https://api.bluelytics.com.ar/v2/latest')
     .then(response => response.json())
     .then(data => {
@@ -37,6 +83,7 @@ fetch('https://api.bluelytics.com.ar/v2/latest')
         contenedor.innerHTML = contenidoHTML;
     });
 
+    //agregarle tarjetas de bootstrap X
 fetch('https://newsdata.io/api/1/news?' +
       'apikey=pub_872920f1d2e2930ecec4fe73de047e2b520e8' +
       '&q=dólar' +
